fb66eaff974dd21ce302cbe5f51be41884b3efdf
Torin Hawkins <torincyie13@gmail.com>
2022-03-22 21:16:11 -0500

    Add project files.

---
+++ ex7_3/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ex7_3
+{
+    partial class frmPubs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPubs));
+            this.dlgPreview = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // dlgPreview
+            // 
+            this.dlgPreview.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.dlgPreview.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.dlgPreview.ClientSize = new System.Drawing.Size(400, 300);
+            this.dlgPreview.Enabled = true;
+            this.dlgPreview.Icon = ((System.Drawing.Icon)(resources.GetObject("dlgPreview.Icon")));
+            this.dlgPreview.Name = "dlgPreview";
+            this.dlgPreview.Visible = false;
+            // 
+            // frmPubs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmPubs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Publishers Listing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmPubs_FormClosing);
+            this.Load += new System.EventHandler(this.frmPubs_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog dlgPreview;
+    }
+}
+
---
+++ ex7_3/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Printing;
+using System.IO;
+
+namespace ex7_3
+{
+    public partial class frmPubs : Form
+    {
+        SqlConnection booksConnection;
+        SqlCommand publishersCommand;
+        SqlDataAdapter publishersAdapter;
+        DataTable publishersTable;
+        int pageNumber;
+        const int recordsPerPage = 6;
+
+        public frmPubs()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmPubs_Load(object sender, EventArgs e)
+        {
+            // Build interface
+            Button[] btnLetter = new Button[27];
+            int l = 0;
+                int w = Convert.ToInt32(this.ClientRectangle.Width / 3);
+            int t = 0;
+            int h = Convert.ToInt32(this.ClientRectangle.Height / 9);
+            // resize form to fit buttons exactly
+            this.ClientSize = new Size(3 * w, 9 * h);
+            for (int i = 0; i < 27; i++)
+            {
+                btnLetter[i] = new Button();
+                btnLetter[i].Name = i.ToString();
+                btnLetter[i].Left = l;
+                btnLetter[i].Width = w;
+                btnLetter[i].Top = t;
+                btnLetter[i].Height = h;
+                btnLetter[i].Text = (char)(i + 65) + " Publishers";
+                this.Controls.Add(btnLetter[i]);
+                btnLetter[i].Click += new EventHandler(this.btnLetter_Click);
+                t += h;
+                if (i == 8 || i == 17)
+                {
+                    l += w;
+                    t = 0;
+                }
+            }
+            btnLetter[26].Text = "Other Publishers";
+            string path = Path.GetFullPath("SQLBooksDB.mdf");
+            // connect to books database
+            booksConnection = new
+                SqlConnection("Data Source=.\\SQLEXPRESS; AttachDBFilename=" + path + ";" +
+                "Integrated Security=True; Connect Timeout=30; User Instance=True");
+
+        }
+
+        private void frmPubs_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // dispose of object
+            booksConnection.Close();
+            booksConnection.Dispose();
+            publishersCommand.Dispose();
+            publishersAdapter.Dispose();
+            publishersTable.Dispose();
+        }
+
+        private void btnLetter_Click(object sender, EventArgs e)
+        {
+            Button whichButton = (Button) sender;
+            // Retrieve records
+            string sql = "SELECT * FROM Publishers ";
+            int i = Convert.ToInt32(whichButton.Name);
+            if (i >= 0 && i <= 24) // A to Y
+            {
+                sql += "WHERE Name >= '" + (char) (i + 65) + "' AND Name < '" + (char) (i + 65 + 1) + "'";
+            }
+            else if (i == 25) // Z
+            {
+                sql += "WHERE Name >= 'Z'";
+            }
+            else // Other
+            {
+                sql += "WHERE Name < 'A'";
+            }
+            sql += " ORDER BY Name";
+            publishersCommand = new SqlCommand(sql, booksConnection);
+            // establish data adapter/data table
+            publishersAdapter = new SqlDataAdapter();
+            publishersAdapter.SelectCommand = publishersCommand;
+            publishersTable = new DataTable();
+            publishersAdapter.Fill(publishersTable);
+            // set up printdocument
+            PrintDocument publishersDocument;
+            // create the document and name it
+            publishersDocument = new PrintDocument();
+            publishersDocument.DocumentName = "Publishers Listing";
+            // add code handler
+            publishersDocument.PrintPage += new PrintPageEventHandler(this.PrintPublishersPage);
+            // print document
+            pageNumber = 1;
+            dlgPreview.Document = publishersDocument;
+            dlgPreview.ShowDialog();
+            // dispose of object when done printing
+            publishersDocument.Dispose();
+        }
+
+        private void PrintPublishersPage(object sender, PrintPageEventArgs e)
+        {
+            // print headings
+            Font myFont = new Font("Arial", 18, FontStyle.Bold);
+            int y = Convert.ToInt32(e.MarginBounds.Top);
+           
+        }
+    }
+}
